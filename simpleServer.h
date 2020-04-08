4585feb79d6eb6a157acb517be90253624b2031f
tug84792 <47865723+tug84792@users.noreply.github.com>
2020-04-08 08:50:49 -0400

    Update simpleServer.h

--- simpleServer.h
+++ simpleServer.h
@@ -1,50 +1,13 @@
-#ifndef PROJECT3_SIMPLESERVER_H
-#define PROJECT3_SIMPLESERVER_H
+
+#ifndef FINALSPELL_SIMPLESERVER_H
+#define FINALSPELL_SIMPLESERVER_H
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <cstring>
-#include <string>
 #include <netinet/in.h>
 #include <cstdio>
 #include <unistd.h>
 #include <cstdlib>
 #define BUF_LEN 512
-#include <fstream>
-//moved "using namespace std;" to after this header in order to avoid a warning.
-#include <iostream>
-using namespace std;
-#include <vector>
-#include <map>
-#include <arpa/inet.h>
-
 int open_listenfd(int);
-
-string dictionary = "dictionary.txt";
-string logText  = "log.txt";
-
-//Map will be used for easier iterations and vectors will be used as a storage data structure
-map <string, int> inputWord;
-vector<int> cSockVector;
-//Socket mutex
-pthread_mutex_t lockForSocket;
-
-//This function is quite simple. It loads the dictionary text file into the program.
-//It is then placed into a data structure for further comparison. It will be used for both simpleServer
-//and simpleClient.
-void initDictionary(void){
-    //Bring in file pointer and read it in
-    fstream dictFP;
-    string localString;
-    dictFP.open(dictionary, ios::in);
-
-    //Grab each word line by line and stored in localString
-    if(dictFP.is_open()){
-        while(getline(dictFP, localString)){
-            inputWord[localString] = 1;
-        }
-    }
-    //Close up the dictionary file
-    dictFP.close();
-}
-
-#endif //PROJECT3_SIMPLESERVER_H
+#endif //FINALSPELL_SIMPLESERVER_H
